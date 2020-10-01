75e3405814e075e4df575599d83a02b0ecc415cc
xuyunbei <xuyunbei@pku.edu.cn>
2020-09-30 21:31:12 -0400

    Delete Proj.m

--- TVL1/Proj.m
+++
@@ -1,9 +0,0 @@
-% Projection function used in l1 based robust regression
-function[y] = Proj(x)
-% x is a column vector
-%
-%[k,~]=size(x);
-%for i = 1:k
-    %y(i) = max(-1,min(1,x(i)));
-%end
-y=max(-1,min(1,x));
